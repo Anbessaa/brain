151ef202ad6d87140aa4d28ed123743da2976cbf
Anbessaa <161745072+Anbessaa@users.noreply.github.com>
2024-07-08 14:31:50 +0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,22 +1,20 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # Your bot token here
 BOT_TOKEN = '6589753473:AAEJFp8iYg8Jm5rUWKN2Q-chlYo6OTc-pUg'
 GAME_URL = 'https://anbessaa.github.io/brain/'
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Welcome to the Game! Click the link below to start playing:')
-    update.message.reply_text(GAME_URL)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Welcome to the Game! Click the link below to start playing:')
+    await update.message.reply_text(GAME_URL)
 
 def main():
-    updater = Updater(BOT_TOKEN)
-    dispatcher = updater.dispatcher
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    dispatcher.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("start", start))
 
-    updater.start_polling()
-    updater.idle()
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
